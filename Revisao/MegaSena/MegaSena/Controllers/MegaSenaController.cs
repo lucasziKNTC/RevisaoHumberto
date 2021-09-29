470073210ecb490bdb3618a578a61c3dd9eaa891
lucasziKNTC <69487662+lucasziKNTC@users.noreply.github.com>
2021-09-29 18:35:54 -0300

    Add files via upload

---
+++ Revisao/MegaSena/MegaSena/Controllers/MegaSenaController.cs
@@ -0,0 +1,93 @@
+﻿using MegaSena.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaSena.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MegaSenaController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public ActionResult Ola()
+        {
+
+            return Ok("Ola");
+
+        }
+
+
+        [HttpPost("RegistrarJogo")]
+        [ProducesResponseType(typeof(Numeros), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+
+
+        public ActionResult RegistrarJogo(Numeros numeros)
+        {
+            
+
+            if(numeros.Numero1 != numeros.Numero2 &&
+               numeros.Numero1 != numeros.Numero3 &&
+               numeros.Numero1 != numeros.Numero4 &&
+               numeros.Numero1 != numeros.Numero5 &&
+               numeros.Numero1 != numeros.Numero6 &&
+
+               numeros.Numero2 != numeros.Numero3 &&
+               numeros.Numero2 != numeros.Numero4 &&
+               numeros.Numero2 != numeros.Numero5 &&
+               numeros.Numero2 != numeros.Numero6 &&
+
+               numeros.Numero3 != numeros.Numero4 &&
+               numeros.Numero3 != numeros.Numero5 &&
+               numeros.Numero3 != numeros.Numero6 &&
+
+               numeros.Numero4 != numeros.Numero5 &&
+               numeros.Numero4 != numeros.Numero6 &&
+
+               numeros.Numero5 != numeros.Numero6 
+     
+              )
+
+            {
+                return Ok(numeros);
+                    
+            }
+
+
+            return BadRequest("Numeros não podem repetir");
+
+
+        }
+
+
+        [HttpPost("RegistrarJogoAleatorio")]
+        [ProducesResponseType(typeof(Numeros), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+
+        public ActionResult RegistrarJogoAleatorio()
+        {
+
+            Random Aleatorio = new Random();
+            string Resultado = "";
+            
+            for(int i=0; i<= 6 - 1; i++)
+            {
+                Resultado = Resultado + " - " + Aleatorio.Next(1 , 60);
+
+                
+            }
+          
+            return Ok("Jogo registrado, seu jogo sera:" + Resultado);
+        }
+        
+       
+    }
+}
---
+++ Revisao/MegaSena/MegaSena/models/Numeros.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaSena.models
+{
+    public class Numeros
+    {
+
+        public int Numero1 { get; set; }
+
+        public int Numero2 { get; set; }
+
+        public int Numero3 { get; set; }
+
+        public int Numero4 { get; set; }
+
+        public int Numero5 { get; set; }
+
+        public int Numero6 { get; set; }
+
+
+    }
+}
